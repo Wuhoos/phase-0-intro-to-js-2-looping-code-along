4c3c5e963ec0f6acb13feaa3cb4c8b7cb4e7c0ea
David <wjw3298@gmail.com>
2023-06-22 18:52:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, surprise) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return messages;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0){
+        console.log(countDown);
+        countDown--;
+    }
+}
